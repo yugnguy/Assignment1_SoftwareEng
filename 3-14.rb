0833d17158caf444283fd435b4e2e4df5e047f15
Phasit Sangklub <52444214+Tcydus@users.noreply.github.com>
2021-08-23 02:24:01 +0700

    change experiment

--- 3-14.rb
+++ 3-14.rb
@@ -1,43 +1,48 @@
 #Project 3.14.
-#Project 3.14.
-
 def my_swap(a,b)
-  puts "#{a} -> #{b}"
   b,a = a,b
-  puts "#{a} -> #{b}"
 end
 
 class Foo
 	attr_accessor :a, :b
   def my_swap_2()
-    puts "----"
-	puts "#{@a} -> #{@b}"
     @b,@a = @a,@b
-	puts "#{@a} -> #{@b}"
-	puts "----"
   end
 end
 
 def my_string_replace_1(s)
-  puts s.gsub( /Hi/, 'Hello') 
+  s.gsub( /Hi/, 'Hello') 
 end
 
 def my_string_replace_2(s)
-  puts s.gsub!( /Hi/, 'Hello')
+  s.gsub!( /Hi/, 'Hello')
 end
 
-my_swap(5,1)
+a,b = 5,1
+puts "----"
+puts "input before my_swap #{a},#{b}"
+my_swap(a,b)
+puts "input after my_swap #{a},#{b}"
+puts "----"
+
 f = Foo.new()
 f.a = 2
 f.b = 7
+puts "----"
+puts "input before my_swap_2 #{f.a},#{f.b}"
 f.my_swap_2
+puts "input after my_swap_2 #{f.a},#{f.b}"
+puts "----"
 
-a = "NotChange->Hi"
-puts "input before method1:#{a}"
+puts "----"
+a = "Hi"
+puts "input before my_string_replace_1: #{a}"
 my_string_replace_1(a)
-puts "input after method1: #{a} "
-puts ""
-a = "Change->Hi"
-puts "input before method2:#{a}"
+puts "input after my_string_replace_1: #{a} "
+puts "----"
+puts "----"
+a = "Hi"
+puts "input before my_string_replace_2: #{a}"
 my_string_replace_2(a)
-puts "input after method2: #{a}"
+puts "input after my_string_replace_2: #{a}"
+puts "----"
